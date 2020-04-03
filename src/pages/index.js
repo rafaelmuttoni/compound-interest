d5f042446fbdc8e9b306588205cd1406b65f548b
Rafael Muttoni <rmuttonic@gmail.com>
2020-04-03 12:49:20 -0300

    Added calculator to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,12 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import Calculator from "../components/Calculator"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>This is Muttoni's starter for Gatsby Projects.</h1>
+    <Calculator />
   </Layout>
 )
 
