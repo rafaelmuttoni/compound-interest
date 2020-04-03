be5e6b385de303d002c41fdfaeab183691a49cdc
Rafael Muttoni <rmuttonic@gmail.com>
2020-04-02 21:09:05 -0300

    Created Calculator component

---
+++ src/components/Calculator/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import * as S from './styled'
+
+const Calculator = () => {
+
+  return (
+    <S.CalculatorWrapper>
+      
+    </S.CalculatorWrapper>
+  )
+}
+
+export default Calculator
