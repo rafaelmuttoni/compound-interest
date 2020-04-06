cfbebe44cea23a5b15d4fc9916535acfe5b33af9
Rafael Muttoni <rmuttonic@gmail.com>
2020-04-06 16:23:01 -0300

    Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Here you type the website's title.`,
-    description: `Here you type the website's description.`,
-    author: `@muttonidigital`,
+    title: `Calculadora de Juros Compostos`,
+    description: `Calcule o resultado dos seus investimentos.`,
+    author: `@rafaelmuttoni`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
@@ -19,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Calculadora Juros Compostos`,
+        short_name: `Juros Compostos`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
